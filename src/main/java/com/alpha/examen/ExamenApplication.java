d690bee917fabe93a7bc27f7aa62e1bef38e4ed7
Lx <adri-amoor@hotmail.com>
2026-02-03 19:16:50 -0600

    1.5 versión Mejoras

--- src/main/java/com/alpha/examen/ExamenApplication.java
+++ src/main/java/com/alpha/examen/ExamenApplication.java
@@ -14,4 +14,18 @@ public class ExamenApplication {
     //http://localhost:8080/student?registro=5 -- crear usuaurios
 
     //http://localhost:8080/student/allRegisters -- conseguir info d usuarios
+
+    //http://localhost:8080/student/update/{nombre} -- actualizar por nombre
+    /*
+    {
+    "nombre": "Mercier",
+    "apellido": "Laurent",
+    "email": "marilou.laurent@example.com",
+    "phone": "05-16-34-30-02",
+    "cell": "06-36-47-46-11",
+    "genero": "male"
+}
+     */
+
+    //http://localhost:8080/student/delete/{id} -- borrar por ID
 }
